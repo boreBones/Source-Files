fd6c2a5ec5ad217502ecbbb2bee9338501263ded
boreBones <zafan.syed@gmail.com>
2016-10-27 10:16:27 -0400

    Add files via upload

---
+++ T_R_C_F_Final_Project/T_R_C_F_Final_Project/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace T_R_C_F_Final_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cmbCat = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbPriority = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbStatus = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.cmdInsert = new System.Windows.Forms.Button();
+            this.cmdUpdate = new System.Windows.Forms.Button();
+            this.cmdDelete = new System.Windows.Forms.Button();
+            this.cmdSort = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pickDate = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // cmbCat
+            // 
+            this.cmbCat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCat.FormattingEnabled = true;
+            this.cmbCat.Items.AddRange(new object[] {
+            "School",
+            "Work",
+            "Family",
+            "Friends",
+            "Other"});
+            this.cmbCat.Location = new System.Drawing.Point(12, 63);
+            this.cmbCat.Name = "cmbCat";
+            this.cmbCat.Size = new System.Drawing.Size(121, 21);
+            this.cmbCat.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Categories";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Enabled = false;
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Task";
+            // 
+            // txtTask
+            // 
+            this.txtTask.ContextMenuStrip = this.contextMenuStrip1;
+            this.txtTask.Location = new System.Drawing.Point(12, 139);
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(131, 20);
+            this.txtTask.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Priority";
+            // 
+            // cmbPriority
+            // 
+            this.cmbPriority.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPriority.FormattingEnabled = true;
+            this.cmbPriority.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.cmbPriority.Location = new System.Drawing.Point(12, 208);
+            this.cmbPriority.Name = "cmbPriority";
+            this.cmbPriority.Size = new System.Drawing.Size(35, 21);
+            this.cmbPriority.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Status";
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbStatus.FormattingEnabled = true;
+            this.cmbStatus.Items.AddRange(new object[] {
+            "Not Started",
+            "In Progress",
+            "Completed"});
+            this.cmbStatus.Location = new System.Drawing.Point(12, 290);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(94, 21);
+            this.cmbStatus.TabIndex = 7;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(198, 29);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(267, 225);
+            this.listBox1.TabIndex = 8;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // cmdInsert
+            // 
+            this.cmdInsert.Location = new System.Drawing.Point(198, 276);
+            this.cmdInsert.Name = "cmdInsert";
+            this.cmdInsert.Size = new System.Drawing.Size(75, 23);
+            this.cmdInsert.TabIndex = 9;
+            this.cmdInsert.Text = "Insert";
+            this.cmdInsert.UseVisualStyleBackColor = true;
+            this.cmdInsert.Click += new System.EventHandler(this.cmdInsert_Click);
+            // 
+            // cmdUpdate
+            // 
+            this.cmdUpdate.Location = new System.Drawing.Point(296, 276);
+            this.cmdUpdate.Name = "cmdUpdate";
+            this.cmdUpdate.Size = new System.Drawing.Size(75, 23);
+            this.cmdUpdate.TabIndex = 10;
+            this.cmdUpdate.Text = "Update";
+            this.cmdUpdate.UseVisualStyleBackColor = true;
+            this.cmdUpdate.Click += new System.EventHandler(this.cmdUpdate_Click);
+            // 
+            // cmdDelete
+            // 
+            this.cmdDelete.Location = new System.Drawing.Point(390, 276);
+            this.cmdDelete.Name = "cmdDelete";
+            this.cmdDelete.Size = new System.Drawing.Size(75, 23);
+            this.cmdDelete.TabIndex = 11;
+            this.cmdDelete.Text = "Delete";
+            this.cmdDelete.UseVisualStyleBackColor = true;
+            this.cmdDelete.Click += new System.EventHandler(this.cmdDelete_Click);
+            // 
+            // cmdSort
+            // 
+            this.cmdSort.Location = new System.Drawing.Point(198, 315);
+            this.cmdSort.Name = "cmdSort";
+            this.cmdSort.Size = new System.Drawing.Size(75, 35);
+            this.cmdSort.TabIndex = 12;
+            this.cmdSort.Text = "Sort by Category";
+            this.cmdSort.UseVisualStyleBackColor = true;
+            this.cmdSort.Click += new System.EventHandler(this.cmdSort_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 326);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Due Date";
+            // 
+            // pickDate
+            // 
+            this.pickDate.Location = new System.Drawing.Point(12, 346);
+            this.pickDate.Name = "pickDate";
+            this.pickDate.Size = new System.Drawing.Size(149, 20);
+            this.pickDate.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 378);
+            this.Controls.Add(this.pickDate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cmdSort);
+            this.Controls.Add(this.cmdDelete);
+            this.Controls.Add(this.cmdUpdate);
+            this.Controls.Add(this.cmdInsert);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.cmbStatus);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbPriority);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTask);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbCat);
+            this.Name = "Form1";
+            this.Text = "To Do Application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbCat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbPriority;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbStatus;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button cmdInsert;
+        private System.Windows.Forms.Button cmdUpdate;
+        private System.Windows.Forms.Button cmdDelete;
+        private System.Windows.Forms.Button cmdSort;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker pickDate;
+
+    }
+}
+
---
+++ T_R_C_F_Final_Project/T_R_C_F_Final_Project/Form1.cs
@@ -0,0 +1,262 @@
+﻿/*
+ * To Do Application
+ * By: Trevor Kinsie, Chris Goguen, Robert Muir, Faizan Syed
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Collections;
+
+
+namespace T_R_C_F_Final_Project
+{
+    public partial class Form1 : Form
+    {
+        private String path = "Tasks.txt";
+        private int index = -1;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            txtTask.KeyPress += txtTask_KeyPress;
+            
+
+            cmbCat.SelectedIndex = 0;
+            cmbPriority.SelectedIndex = 0;
+            cmbStatus.SelectedIndex = 0;
+
+
+            if (File.Exists(path))
+            {
+                readFile();
+            }
+
+            setControlState("i");
+        }
+
+        //Will check for 
+        void txtTask_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char c = e.KeyChar;
+
+            // delim being pressed    : 
+            // any characters that are not a-z A-Z 0-9 or spaces
+            int len = ((TextBox)sender).Text.Length;
+            ((TextBox)sender).SelectionStart = len;
+
+            if (c != 8)
+            {
+                if ((c < 65 || c > 90) && (c < 97 || c > 122) && (c < 48 || c > 57) && (c < 32 || c > 32))
+                {
+                    e.Handled = true;
+                }
+            }
+        
+        
+        }
+
+        //dataGood will check if the task textfield is not empty
+        private bool dataGood()
+        {
+            // is that stuff good?
+            if (txtTask.Text.Length < 1)
+            {
+                MessageBox.Show("A Task is required to  contain more than 1 character");
+                txtTask.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        private void setControlState(String text)
+        {
+            if(text.Equals("i"))
+            {
+                cmdInsert.Enabled = true;
+                cmdDelete.Enabled = false;
+                cmdUpdate.Enabled = false;
+                cmdSort.Enabled = false;
+                clearText();
+            }
+
+            else if (text.Equals("u/d"))
+            {
+                cmdInsert.Enabled = false;
+                cmdDelete.Enabled = true;
+                cmdUpdate.Enabled = true;
+                cmdSort.Enabled = true;
+              //  clearText();
+            }
+}
+
+        private void clearText()
+        {
+            // clear the 'input' fields
+           txtTask.Text = "";
+           
+        }
+
+        private void readFile()
+        {
+            //read the file
+            listBox1.Items.Clear();
+            StreamReader noteReader = new StreamReader(path);
+
+            string record = noteReader.ReadLine();
+
+            while (record != null)
+            
+            {
+                listBox1.Items.Add(record);
+                record = noteReader.ReadLine();
+
+            }
+            noteReader.Close();
+
+        }
+
+        private void cmdInsert_Click(object sender, EventArgs e)
+        {
+          
+            // Grabs variables
+            string selectedCategory = this.cmbCat.GetItemText(this.cmbCat.SelectedItem);
+            string task = txtTask.Text;
+            string selectedPriority = this.cmbPriority.GetItemText(this.cmbPriority.SelectedItem);
+            string selectedStatus = this.cmbStatus.GetItemText(this.cmbStatus.SelectedItem);
+            string selectedDate = pickDate.Value.ToShortDateString();
+
+            if (dataGood())
+            {
+                //Will write the data into the file
+                StreamWriter noteWriter = new StreamWriter(path, true);
+
+                noteWriter.WriteLine(selectedCategory +":" + task + ":"+selectedPriority+":"+selectedStatus+":"+selectedDate);
+                noteWriter.Close();
+                readFile();
+                clearText();
+            }
+            setControlState("i");
+        
+        }
+
+        //Update will update the fields and the textfile
+        private void cmdUpdate_Click(object sender, EventArgs e)
+        {
+            string selectedDate = pickDate.Value.ToShortDateString();
+            if (dataGood())
+            {
+                File.Delete(path);
+                StreamWriter sw = new StreamWriter(path, true);
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    if (i != index)
+                    {
+                        sw.WriteLine(listBox1.Items[i].ToString());
+                    }
+                    else
+                    {
+                        sw.WriteLine(cmbCat.Text + ":" + txtTask.Text + ":" + cmbPriority.Text + ":" + cmbStatus.Text + ":" + selectedDate);
+                    }
+                }
+
+                sw.Close();
+                readFile();
+            }
+            setControlState("i");
+        }
+
+        //Delete will delete the record
+        private void cmdDelete_Click(object sender, EventArgs e)
+        {
+
+            //Will only delete the record if the user allow it
+               if (MessageBox.Show("Are you sure you want want to delete this record?","Confirm Record Delete",MessageBoxButtons.YesNo,MessageBoxIcon.Warning
+                   ,MessageBoxDefaultButton.Button2) == System.Windows.Forms.DialogResult.Yes)
+               {
+                   File.Delete(path);
+                   StreamWriter myWriter = new StreamWriter(path, true);
+                   for ( int x = 0 ; x < listBox1.Items.Count ; x++)
+                   {
+                       if ( x != index)
+                       {
+                           myWriter.WriteLine(listBox1.Items[x].ToString());
+                       }
+                   }
+                   myWriter.Close();
+                   readFile();
+               }
+               setControlState("i");
+               readFile(); 
+        }
+
+        //Will select a record a put the values into the textboxes,etc
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            index = listBox1.SelectedIndex;
+            if (index > -1)
+            {
+                string record = listBox1.Items[index].ToString();
+                char[] delims = {':'};
+                string[] tokens = record.Split(delims);
+                cmbCat.Text = tokens[0];
+                txtTask.Text = tokens[1];
+                cmbPriority.Text = tokens[2];
+                cmbStatus.Text = tokens[3];
+                pickDate.Text = tokens[4];
+                setControlState("u/d");
+            }
+        }
+
+        //The sort function will sort the tasks based on category
+        private void cmdSort_Click(object sender, EventArgs e)
+        {
+            //ArrayList to hold the temporary values
+            ArrayList myList = new ArrayList();
+
+            //read the file
+            listBox1.Items.Clear();
+
+            StreamReader noteReader = new StreamReader(path);
+
+            string record = noteReader.ReadLine();
+
+            while (record != null)
+            {
+            //Will spit the current record into tokens and then add it to the arraylist
+                string [] temp =record.Split(':');
+                string tempCat = temp[0];
+                string tempTask = temp[1];
+                string tempPri = temp[2];
+                string tempStatus = temp[3];
+                string tempDate = temp[4];
+                String finalText = tempCat + ":" + tempTask + ":" + tempPri + ":" + tempStatus + ":" + tempDate;
+                myList.Add(finalText);
+                record = noteReader.ReadLine();
+            }
+            
+            //Sorts the arraylist alphabetically
+            myList.Sort();
+            for (int x = 0; x < myList.Count; x++)
+            {
+                listBox1.Items.Add(myList[x]);
+
+            }
+                noteReader.Close();
+
+        }
+   
+    }
+}
